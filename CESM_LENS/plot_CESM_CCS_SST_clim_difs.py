288dff04888c1107ab03d92c5425d3581f87230f
amoebaliz <liz.drenkard@gmail.com>
2018-05-10 14:26:03 -0700

    update

--- CESM_LENS/plot_CESM_CCS_SST_clim_difs.py
+++ CESM_LENS/plot_CESM_CCS_SST_clim_difs.py
@@ -5,32 +5,28 @@ import matplotlib.pyplot as plt
 
 A = np.load('/Users/elizabethdrenkard/Desktop/LENS_clim_difs.npy')
 
+avg_A = np.mean(A,axis=0)
+print np.argsort(avg_A)+1
+print avg_A[np.argsort(avg_A)]
+
 for nl in range(A.shape[1]):
-    if nl == (34-1):
-       colr = 'red'; zord = 4
+    if nl == (17-1):
+       colr = 'red'; mec_clr = 'firebrick'; zord = 4
     elif nl == (19-1):
-       colr = 'blue'; zord = 4
-    elif nl == (11-1):
-       colr = 'green'; zord = 4 
-    elif nl == (17-1):
-       colr = 'black'; zord = 4
-    elif nl == (18-1):
-       colr = 'purple'; zord = 4
+       colr = 'blue'; mec_clr = 'darkblue'; zord = 4
 
-    elif nl == (24-1):
-       colr = 'pink'; zord = 4
+    # COMPARITIVE COLOR
+    # elif nl == (34-1):
+    #   colr = 'cyan'; zord = 4 
+
     else:
-       colr = 'grey'; zord = 2
+       colr = 'silver'; mec_clr = 'darkgrey'; zord = 2
 
 
-    plt.plot(np.arange(1,12+1),A[:,nl],color=colr,marker='o',zorder=zord)
+    plt.plot(np.arange(1,12+1),A[:,nl],marker='o', ms=10, mfc=colr,mec=mec_clr,ls='None',zorder=zord)
 
 
-plt.figure()
-plt.plot(np.arange(1,12+1),A[:,15-1],color='blue',marker='o',zorder=2)
-plt.plot(np.arange(1,12+1),A[:,34-1],color='green',marker='o',zorder=2)
-plt.plot(np.arange(1,12+1),A[:,24-1],color='purple',marker='o',zorder=2)
-plt.plot(np.arange(1,12+1),A[:,17-1],color='olive',marker='o',zorder=2)
-plt.plot(np.arange(1,12+1),A[:,13-1],color='red',marker='o',zorder=2)
+labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG','SEP','OCT','NOV','DEC']
+plt.xticks(np.arange(1,12+1),labels)
 plt.show()
 
