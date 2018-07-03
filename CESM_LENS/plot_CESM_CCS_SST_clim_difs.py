2ad09d46f502419042b719b65d8ef2f952509256
amoebaliz <liz.drenkard@gmail.com>
2018-07-02 19:31:57 -0700

    validating ROMS against satellite data

--- CESM_LENS/plot_CESM_CCS_SST_clim_difs.py
+++ CESM_LENS/plot_CESM_CCS_SST_clim_difs.py
@@ -12,8 +12,8 @@ fig = plt.figure(figsize=(10,4))
 for nl in range(A.shape[1]):
     if nl == (17-1):
        colr = 'red'; mec_clr = 'firebrick'; zord = 4
-#    elif nl == (19-1):
-#       colr = 'blue'; mec_clr = 'darkblue'; zord = 4
+    elif nl == (19-1):
+       colr = 'blue'; mec_clr = 'darkblue'; zord = 4
 
     # COMPARITIVE COLOR
     # elif nl == (34-1):
@@ -28,6 +28,6 @@ for nl in range(A.shape[1]):
 
 labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG','SEP','OCT','NOV','DEC']
 plt.xticks(np.arange(1,12+1),labels)
-plt.savefig('fig2')
+plt.savefig('fig0')
 plt.show()
 
